ef833290441ba84f7585d2309868db25ceb6f6ce
Pratik <68808278+Pratikmourya@users.noreply.github.com>
2022-08-18 23:10:27 +0530

    Added backend login register modules.

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import expressAsyncHandler from 'express-async-handler';
+import User from '../models/userModel.js';
+import { isAuth, isAdmin, generateToken } from '../util.js';
+
+const userRouter = express.Router();
+
+
+//Get all users  
+userRouter.get('/', expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
+
+//User Signup/Register
+userRouter.post('/signup', expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
+  })
+);
+
+//User login
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user),
+        });
+        return;
+      }
+    }
+    res.status(401).send({ message: 'Invalid email or password' });
+  })
+);
+
+export default userRouter;
