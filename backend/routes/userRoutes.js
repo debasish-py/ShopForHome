4cca1f27f9d44b6805e576c298d91fd5c9e74997
Pratik <68808278+Pratikmourya@users.noreply.github.com>
2022-08-19 14:31:05 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import bcrypt from 'bcryptjs';
-import expressAsyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import { isAuth, isAdmin, generateToken } from '../util.js';
+const express = require("express")
+const bcrypt = require("bcrypt")
+const expressAsyncHandler = require('express-async-handler')
+const User = require('../models/userModel.js')
+//import {generateToken } from '../util.js';
+const jwt=require("jsonwebtoken")
 
 const userRouter = express.Router();
 
@@ -15,40 +16,44 @@ userRouter.get('/', expressAsyncHandler(async (req, res) => {
 );
 
 //User Signup/Register
-userRouter.post('/signup', expressAsyncHandler(async (req, res) => {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password),
-    });
-    const user = await newUser.save();
-    res.send({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user),
-    });
+userRouter.post('/signup',async (req, res) => {
+  const data = req.body
+  const hashedpassword=await bcrypt.hash(data.password,4)
+    //const generatedtoken=jwt.sign(jwt.sign({_id: user._id,name: user.name,email: user.email,isAdmin: user.isAdmin,},{expiresIn:'1h'}))
+    const result = await User.create({
+      name:data.name,
+      email:data.email,
+      password:hashedpassword
   })
+  res.status(201).send("user registered successfully") 
+    
+  }
 );
 
 //User login
 userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
-    const user = await User.findOne({ email: req.body.email });
-    if (user) {
-      if (bcrypt.compareSync(req.body.password, user.password)) {
-        res.send({
-          _id: user._id,
-          name: user.name,
-          email: user.email,
-          isAdmin: user.isAdmin,
-          token: generateToken(user),
-        });
-        return;
-      }
+    const data=req.body
+    const user=await User.findOne({email:data.email})
+   // const username = await UserModel.findOne({uname:data.email})
+    if(user){
+    const comparison=await bcrypt.compare(data.password,user.password)
+    if(comparison){
+      const generatedtoken=jwt.sign({_id: user._id,name: user.name,email: user.email,isAdmin: user.isAdmin,},'adeddse',{expiresIn:'1h'})
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generatedtoken
+      })
     }
-    res.status(401).send({ message: 'Invalid email or password' });
-  })
-);
+    else{
+      res.status(401).send({ message: 'Invalid email/password' })
+    }
+  }
+  else{
+    res.status(401).send({ message: 'Invalid email/password' })
+  }
+}));
 
-export default userRouter;
+module.exports = userRouter;
