4101ed069a676148d11c945130c30ce7a4b0b142
Pratik <68808278+Pratikmourya@users.noreply.github.com>
2022-08-20 18:42:56 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,36 +4,46 @@ const expressAsyncHandler = require('express-async-handler')
 const User = require('../models/usermodel.js')
 const Product = require('../models/productmodel.js')
 const usercontrollers = require('../controllers/usercontrollers')
+const productcontrollers = require('../controllers/productcontrollers')
 //import {generateToken } from '../util.js';
 const jwt=require("jsonwebtoken")
 
 const userRouter = express.Router();
 
-/*const generateToken = (user) => {
-  return jwt.sign(
-    {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: '10d'
-    }
-  );
-}; */
-
+//USER OPERATIONS
 //Get all users  
 userRouter.get("/",usercontrollers.getusers)
 
-//User Signup/Register
+//Get specific user with id/name
+userRouter.get('/getuser/:name', usercontrollers.getuser)
+
+//User Signup/Register Create
 userRouter.post('/signup', usercontrollers.signup)
 
-//User login
+//User login 
 userRouter.post('/signin',usercontrollers.signin)
 
-//search
+//Update user with id/name
+userRouter.patch('updateuser/:name',usercontrollers.updateuser)
+
+//Delete User with id/name
+userRouter.delete('deleteuser/:name',usercontrollers.deleteuser)
+ 
+//search with name
 userRouter.get('/search/:name',usercontrollers.search)
+
+//PRODUCT OPERATIONS
+
+//Get specific product with name
+userRouter.get('/getproduct/:name',productcontrollers.getproduct)
+
+//Create Product
+userRouter.post('/createproduct',productcontrollers.createproduct)
+
+//Update product with resp name
+userRouter.patch('/updateproduct/:name',productcontrollers.updateproduct)
+
+//Delete Product with resp name
+userRouter.delete('/deleteproduct/:name',productcontrollers.deleteproduct)
 
 module.exports = userRouter;
