c60cd9be5df3028e80cc8bb82357acdcbdab5dff
Pratik <68808278+Pratikmourya@users.noreply.github.com>
2022-08-20 22:49:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,18 @@
 const express=require("express")
 const app=express()
+const cors=require("cors")
 const mongoose=require("mongoose")
-const userRouter = require('./routes/userRoutes')
+
+const userRouter = require('./routes/uRoutes')
+
 const bodyParser=require("body-parser")
+app.use(cors())
 app.use(bodyParser.json())
 app.use('/', userRouter);
+
+
 mongoose.connect("mongodb://localhost:27017/sfhome").then(() => {console.log('connected to db');}).catch((err) => {console.log(err.message);});
 
-app.listen(5000, () => {
-    console.log(`serve at http://localhost:5000`);
-  });
+app.listen(3001, () => {
+    console.log(`serve at http://localhost:3001`);
+  })
