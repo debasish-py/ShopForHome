8619fffe7b61b827198e470214fe40d860c5f0f1
md tarikh khan <97981490+DBcoppr@users.noreply.github.com>
2022-08-20 22:33:24 +0530

    included viewproduct and bulkupload

---
+++ backend/routes/uRoutes.js
@@ -0,0 +1,55 @@
+const express = require("express")
+
+const bcrypt = require("bcrypt")
+
+
+const usercontrollers = require('../controllers/usercontrollers')
+const productcontrollers = require('../controllers/productcontrollers')
+//import {generateToken } from '../util.js';
+const jwt=require("jsonwebtoken")
+
+const userRouter = express.Router();
+
+//USER OPERATIONS
+//Get all users  
+userRouter.get("/getalluser",usercontrollers.getusers)
+
+//Get specific user with id/name
+userRouter.get('/getuser/:name', usercontrollers.getuser)
+
+//User Signup/Register Create
+userRouter.post('/signup', usercontrollers.signup)
+
+//User login 
+userRouter.post('/signin',usercontrollers.signin)
+
+//Update user with id/name
+userRouter.patch('updateuser/:name',usercontrollers.updateuser)
+
+//Delete User with id/name
+userRouter.delete('deleteuser/:name',usercontrollers.deleteuser)
+ 
+//search with name
+userRouter.get('/search/:name',usercontrollers.search)
+
+//PRODUCT OPERATIONS
+
+//Get specific product with name
+
+
+userRouter.post("/upload",productcontrollers.bulkUpload)
+
+userRouter.get("/viewProduct",productcontrollers.viewProduct)
+
+userRouter.get('/getproduct/:name',productcontrollers.getproduct)
+
+//Create Product
+userRouter.post('/createproduct',productcontrollers.createproduct)
+
+//Update product with resp name
+userRouter.patch('/updateproduct/:name',productcontrollers.updateproduct)
+
+//Delete Product with resp name
+userRouter.delete('/deleteproduct/:name',productcontrollers.deleteproduct)
+
+module.exports = userRouter;
