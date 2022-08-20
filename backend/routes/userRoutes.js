5ae35f4b5f9deaa032aa8be0ff8b3700c6994f1b
Pratik <68808278+Pratikmourya@users.noreply.github.com>
2022-08-20 13:59:30 +0530

    Update userRoutes.js
    switched with controllers style.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,59 +1,39 @@
 const express = require("express")
 const bcrypt = require("bcrypt")
 const expressAsyncHandler = require('express-async-handler')
-const User = require('../models/userModel.js')
+const User = require('../models/usermodel.js')
+const Product = require('../models/productmodel.js')
+const usercontrollers = require('../controllers/usercontrollers')
 //import {generateToken } from '../util.js';
 const jwt=require("jsonwebtoken")
 
 const userRouter = express.Router();
 
+/*const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '10d'
+    }
+  );
+}; */
 
 //Get all users  
-userRouter.get('/', expressAsyncHandler(async (req, res) => {
-    const users = await User.find({});
-    res.send(users);
-  })
-);
+userRouter.get("/",usercontrollers.getusers)
 
 //User Signup/Register
-userRouter.post('/signup',async (req, res) => {
-  const data = req.body
-  const hashedpassword=await bcrypt.hash(data.password,4)
-    //const generatedtoken=jwt.sign(jwt.sign({_id: user._id,name: user.name,email: user.email,isAdmin: user.isAdmin,},{expiresIn:'1h'}))
-    const result = await User.create({
-      name:data.name,
-      email:data.email,
-      password:hashedpassword
-  })
-  res.status(201).send("user registered successfully") 
-    
-  }
-);
+userRouter.post('/signup', usercontrollers.signup)
 
 //User login
-userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
-    const data=req.body
-    const user=await User.findOne({email:data.email})
-   // const username = await UserModel.findOne({uname:data.email})
-    if(user){
-    const comparison=await bcrypt.compare(data.password,user.password)
-    if(comparison){
-      const generatedtoken=jwt.sign({_id: user._id,name: user.name,email: user.email,isAdmin: user.isAdmin,},'adeddse',{expiresIn:'1h'})
-      res.send({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-        token: generatedtoken
-      })
-    }
-    else{
-      res.status(401).send({ message: 'Invalid email/password' })
-    }
-  }
-  else{
-    res.status(401).send({ message: 'Invalid email/password' })
-  }
-}));
+userRouter.post('/signin',usercontrollers.signin)
+
+//search
+userRouter.get('/search/:name',usercontrollers.search)
 
 module.exports = userRouter;
